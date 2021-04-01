0fd7dc2d889e0af0d50210c7d648580fc21ca6c2
benwang <56830442+samwang141224@users.noreply.github.com>
2021-04-01 14:43:10 +0800

    Update test_preprocess.py

--- desidlas/tests/test_preprocess.py
+++ desidlas/tests/test_preprocess.py
@@ -5,6 +5,7 @@ from desidlas.dla_cnn import defs
 #load basic parameter value 
 REST_RANGE = defs.REST_RANGE
 kernel = defs.kernel
+best_v=defs.best_v
 
 #load modules in preprocess.py
 from desidlas.datasets.preprocess import label_sightline
@@ -14,6 +15,18 @@ from desidlas.datasets.preprocess import estimate_s2n
 from desidlas.datasets.preprocess import generate_summary_table
 from desidlas.datasets.preprocess import write_summary_table
 
+#load DesiMock
+from desidlas.datasets.DesiMock import DesiMock
+
+
+#load spectra file for testing
+spectra='desidlas/tests/datafile/spectra/spectra-16-1375.fits'
+truth='desidlas/tests/datafile/spectra/truth-16-1375.fits'
+zbest='desidlas/tests/datafile/spectra/zbest-16-1375.fits'
+
+#
+specs = DesiMock()
+specs.read_fits_file(spectra,truth,zbest)
 
 def test_label_sightline():
     # Generate sightline
