3c5fdad3375de5250a279108490eff574452d532
benwang <56830442+samwang141224@users.noreply.github.com>
2021-05-12 22:51:46 +0800

    Update datasetting.py

--- desidlas/datasets/datasetting.py
+++ desidlas/datasets/datasetting.py
@@ -76,26 +76,22 @@ def split_sightline_into_samples(sightline, REST_RANGE=REST_RANGE, kernel=kernel
     """
     lam, lam_rest, ix_dla_range = get_lam_data(sightline.loglam, sightline.z_qso, REST_RANGE)
     kernelrangepx = int(kernel/2) #200
-    #samplerangepx = int(kernel*pos_sample_kernel_percent/2) #60
+    
     #padding the sightline:
     flux_padded,lam_padded,pixel_num_left=pad_sightline(sightline,lam,lam_rest,ix_dla_range,kernelrangepx,v=best_v['b'])
      
-    #ix_dlas = [(np.abs(lam[ix_dla_range]-dla.central_wavelength).argmin()) for dla in sightline.dlas]
-    #coldensity_dlas = [dla.col_density for dla in sightline.dlas]       # column densities matching ix_dlas
-
-    # FLUXES - Produce a 1748x400 matrix of flux values
-    #fluxes_matrix = np.vstack(map(lambda x:x[0][x[1]-kernelrangepx:x[1]+kernelrangepx],zip(itertools.repeat(sightline.flux), np.nonzero(ix_dla_range)[0])))
+    
+    
     fluxes_matrix = np.vstack(map(lambda x:x[0][x[1]-kernelrangepx:x[1]+kernelrangepx],zip(itertools.repeat(flux_padded), np.nonzero(ix_dla_range)[0]+pixel_num_left)))
     lam_matrix = np.vstack(map(lambda x:x[0][x[1]-kernelrangepx:x[1]+kernelrangepx],zip(itertools.repeat(lam_padded), np.nonzero(ix_dla_range)[0]+pixel_num_left)))
     #using cut will lose side information,so we use padding instead of cutting 
-    #fluxes_matrix = np.vstack(map(lambda x:x[0][x[1]-kernelrangepx:x[1]+kernelrangepx],zip(itertools.repeat(sightline.flux), np.nonzero(ix_dla_range)[0][cut])))
-    #lam_matrix = np.vstack(map(lambda x:x[0][x[1]-kernelrangepx:x[1]+kernelrangepx],zip(itertools.repeat(lam), np.nonzero(ix_dla_range)[0][cut])))
+    
     #the wavelength and flux array we input:
     input_lam=lam_padded[np.nonzero(ix_dla_range)[0]+pixel_num_left]
     input_flux=flux_padded[np.nonzero(ix_dla_range)[0]+pixel_num_left]
-    # Return
+   
     return fluxes_matrix, sightline.classification, sightline.offsets, sightline.column_density,lam_matrix,input_lam,input_flux
-    #return fluxes_matrix, sightline.classification, sightline.offsets, sightline.column_density
+   
 
 def select_samples_50p_pos_neg(sightline,kernel=kernel):
     """
@@ -113,31 +109,19 @@ def select_samples_50p_pos_neg(sightline,kernel=kernel):
         positive + negative indices
 
     """
-    #classification = data[1]
+   
     lam, lam_rest, ix_dla_range = get_lam_data(sightline.loglam, sightline.z_qso)
-    kernelrangepx = int(kernel/2)
-    #cut=((np.nonzero(ix_dla_range)[0])>=kernelrangepx)&((np.nonzero(ix_dla_range)[0])<=(len(lam)-kernelrangepx-1))
-    #newclassification=sightline.classification[cut]
-    num_pos = np.sum(sightline.classification==1, dtype=np.float64)
-    num_neg = np.sum(sightline.classification==0, dtype=np.float64)
-    n_samples = int(min(num_pos, num_neg))
+    kernelrangepx = int(kernel/2)# take half length of the kernel
+    
+    num_pos = np.sum(sightline.classification==1, dtype=np.float64) #count the quantity of all positive samples(classification=1,with DLAs)
+    num_neg = np.sum(sightline.classification==0, dtype=np.float64)#count the quantity of all negtive samples
+    n_samples = int(min(num_pos, num_neg)) #take the minimum of these two quantities
 
-    r = np.random.permutation(len(sightline.classification))
+    r = np.random.permutation(len(sightline.classification))#make a random array
 
-    pos_ixs = r[sightline.classification[r]==1][0:n_samples]
-    neg_ixs = r[sightline.classification[r]==0][0:n_samples]
-    # num_total = data[0].shape[0]
-    # ratio_neg = num_pos / num_neg
-
-    # pos_mask = classification == 1      # Take all positive samples
-
-    # neg_ixs_by_ratio = np.linspace(1,num_total-1,round(ratio_neg*num_total), dtype=np.int32) # get all samples by ratio
-    # neg_mask = np.zeros((num_total),dtype=np.bool) # create a 0 vector of negative samples
-    # neg_mask[neg_ixs_by_ratio] = True # set the vector to positives, selecting for the appropriate ratio across the whole sightline
-    # neg_mask[pos_mask] = False # remove previously positive samples from the set
-    # neg_mask[classification == -1] = False # remove border samples from the set, what remains is still in the right ratio
-
-    # return pos_mask | neg_mask
+    pos_ixs = r[sightline.classification[r]==1][0:n_samples]# index for positive samples
+    neg_ixs = r[sightline.classification[r]==0][0:n_samples]# index for negative samples
+    
     return np.hstack((pos_ixs,neg_ixs))
-    #return np.hstack(pos_ixs,neg_ixs)
+    
 
