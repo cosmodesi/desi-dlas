e1be3568c4ce7125abceee6e230c15fdf8d89437
benwang <56830442+samwang141224@users.noreply.github.com>
2021-10-03 21:17:18 +0800

    Update test_prediction.py

--- desidlas/prediction/test_prediction.py
+++ desidlas/prediction/test_prediction.py
@@ -33,13 +33,15 @@ savefile='desidlas/tests/datafile/partprediction.npy'
 
 r=np.load(pred_dataset,allow_pickle = True,encoding='latin1').item()
 
-checkpoint_filename='desidlas/prediction/model/train_highsnr/current_99999' #this model should be downloaded from the google drive, the link is in instal.rst
+remote_data = pytest.mark.skipif(os.getenv('CNN_MODEL') is None,
+                                 reason='test requires CNN model')
+@remote_data
+def test_prediction():
+  checkpoint_filename='desidlas/prediction/model/train_highsnr/current_99999' #this model should be downloaded from the google drive, the link is in instal.rst
+  dataset={}
+  for sight_id in r.keys():
+      flux=np.array(r[sight_id]['FLUX'])
+      (pred, conf, offset, coldensity)=predictions_ann(hyperparameters, flux, checkpoint_filename, TF_DEVICE='')
+      dataset[sight_id]={'pred':pred,'conf':conf,'offset': offset, 'coldensity':coldensity }
 
-dataset={}
-
-for sight_id in r.keys():
-    flux=np.array(r[sight_id]['FLUX'])
-    (pred, conf, offset, coldensity)=predictions_ann(hyperparameters, flux, checkpoint_filename, TF_DEVICE='')
-    dataset[sight_id]={'pred':pred,'conf':conf,'offset': offset, 'coldensity':coldensity }
-
-np.save(savefile,dataset)
+  np.save(savefile,dataset)
