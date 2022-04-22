e88a68cea6f845ee16117b4c47c93f43ee0ce0dc
Jiaqi Zou <zoujq20@mails.tsinghua.edu.cn>
2022-04-22 17:22:18 +0800

    Update run_dlafinder_nersc.py

--- run_dlafinder_nersc.py
+++ run_dlafinder_nersc.py
@@ -2,7 +2,15 @@ import numpy as np
 import os
 from desidlas.datasets.get_sightlines import get_sightlines
 from desidlas.prediction.pred_sightline import save_pred
-#u firstly need "conda install tensorflow"
+'''
+To run DLA finder on NERSC
+1. You can use code from this branch: "git clone -b realdata https://github.com/cosmodesi/desi-dlas.git"
+2. Then make sure u have installed Tensorflow. "conda install tensorflow"
+3. Download 3 CNN model files as described in desidlas/prediction/model/train_highsnr/highsnrmodel; desidlas/prediction/model/train_midsnr/midsnrmodel; desidlas/prediction/model/train_lowsnr/lowsnrmodel
+4. Modify desidlas/prediction/get_partprediction_sightline.py to relocate to your folder
+5. Put the py file which you want to run in the same directory as desidlas (Just like this file), then run it to get the DLA catalog.
+'''
+
 #preprocess spectra
 outpath='/global/cfs/cdirs/desi/users/jqzou/test_sightlines.npy'
 zbest='/global/cfs/cdirs/desi/spectro/redux/fuji/healpix/sv3/dark/99/9994/redrock-sv3-dark-9994.fits'
