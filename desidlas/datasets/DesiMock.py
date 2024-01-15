696eefa75254036e0834f734c95d162bc4449bc4
tanting <ttss573561488@outlook.com>
2024-01-15 00:24:54 -0800

    ting

--- desidlas/datasets/DesiMock.py
+++ desidlas/datasets/DesiMock.py
@@ -161,12 +161,20 @@ class DesiMock:
         if camera == 'all':
             get_data()
             # this part is to deal with the overlapping part(only blue channel and the red channel)
+            '''
             blfbound = np.argwhere(abs(self.wavelength - self.wavelength[self.split_point_br])<0.000001)[0][0]
             rrgbound = np.argwhere(abs(self.wavelength - self.wavelength[self.split_point_br-1])<0.000001)[1][0]
             #rzfbound = np.argwhere(abs(self.wavelength - self.wavelength[self.split_point_rz])<0.000001)[0][0]
             #rzgbound = np.argwhere(abs(self.wavelength - self.wavelength[self.split_point_rz-1])<0.000001)[1][0]
             rzfbound = np.argwhere(abs(self.wavelength - self.wavelength[self.split_point_rz])<0.4004)[0][0]
             rzgbound = np.argwhere(abs(self.wavelength - self.wavelength[self.split_point_rz-1])<0.4)[-1][0]
+            '''
+            #for DESI iron ohio mock
+            blfbound = np.argmin(abs(self.wavelength[:self.split_point_br] - self.wavelength[self.split_point_br]) )
+            rrgbound = self.split_point_br + np.argmin(abs(self.wavelength[self.split_point_br:] - self.wavelength[self.split_point_br - 1]))
+            rzfbound = np.argmin(abs(self.wavelength[:self.split_point_rz] - self.wavelength[self.split_point_rz]) )
+            rzgbound = self.split_point_rz + np.argmin(abs(self.wavelength[self.split_point_rz:] - self.wavelength[self.split_point_rz - 1]))+1 #+1 for mock
+            
             overlap_flux_b = sightline.flux[blfbound:self.split_point_br]
             overlap_flux_r = sightline.flux[self.split_point_br:rrgbound+1]
             overlap_error_b = sightline.error[blfbound:self.split_point_br]
